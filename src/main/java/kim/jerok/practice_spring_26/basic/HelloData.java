ebeb1664f4c3ec1032c61a96b9eb3e4a7980bb50
jerok <jerok.kim@gmail.com>
2023-05-13 01:25:58 +0900

    practice spring

---
+++ src/main/java/kim/jerok/practice_spring_26/basic/HelloData.java
@@ -0,0 +1,13 @@
+package kim.jerok.practice_spring_26.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloData {
+
+    private String username;
+    private int age;
+
+}
