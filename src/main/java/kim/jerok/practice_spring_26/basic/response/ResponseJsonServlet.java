615fb30c8f6fda61689a0e700ad9436ab6772446
jerok <jerok.kim@gmail.com>
2023-05-13 01:55:09 +0900

    practice spring

---
+++ src/main/java/kim/jerok/practice_spring_26/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,33 @@
+package kim.jerok.practice_spring_26.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kim.jerok.practice_spring_26.basic.HelloData;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Content-Type: application/json
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        HelloData helloData = new HelloData();
+        helloData.setUsername("jerok");
+        helloData.setAge(20);
+
+        // {"username":"jerok", "age":20}
+        String result = objectMapper.writeValueAsString(helloData);
+        response.getWriter().write(result);
+
+    }
+}
